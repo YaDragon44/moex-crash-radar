2fbdd9f38037b61727c2895f05e4f3cf2035aecf
YaDragon44 <7jr44jtzvy@privaterelay.appleid.com>
2026-09-25 15:29:54 +0300

    VKCO valuation: test sensitivity matrix

--- vkco-monitor/test_valuation.py
+++ vkco-monitor/test_valuation.py
@@ -17,3 +17,9 @@ def test_market_evidence_is_context_only():
     x=valuation.build(113.55,{"issue_size":572904180},{"facts":{"net_debt_bln":60.2,"guidance_ebitda_2026_bln":24}})
     assert x["market_evidence"]["peer_median"]==5.205
     assert x["scenarios"][1]["ev_ebitda_assumption"]==6.0
+
+def test_sensitivity_matrix():
+    x=valuation.build(112.75,{"issue_size":572904180},{"facts":{"net_debt_bln":60.2,"guidance_ebitda_2026_bln":24}})
+    assert [r["ebitda_bln"] for r in x["sensitivity"]]==[24.0,26.0,28.0]
+    assert [v["multiple"] for v in x["sensitivity"][0]["values"]]==[4.0,5.0,6.0,7.0,8.0]
+    assert x["sensitivity"][0]["values"][0]["value_per_share"] < x["sensitivity"][2]["values"][-1]["value_per_share"]
