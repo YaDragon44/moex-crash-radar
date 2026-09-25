731f1fbf997563fe1f91a338cf92afc4893eef9d
YaDragon44 <7jr44jtzvy@privaterelay.appleid.com>
2026-09-25 14:07:25 +0300

    VKCO: test valuation contract

---
+++ vkco-monitor/test_valuation.py
@@ -0,0 +1,8 @@
+import valuation
+def test_valuation_contract():
+    x=valuation.build(113.75,{"issue_size":572904180},{"facts":{"net_debt_bln":60.2,"guidance_ebitda_2026_bln":24}})
+    assert x["status"]=="OK" and x["read_only"] is True
+    assert x["current_ev_ebitda"]>0 and len(x["scenarios"])==3
+    assert x["scenarios"][0]["value_per_share"]<x["scenarios"][1]["value_per_share"]<x["scenarios"][2]["value_per_share"]
+def test_fail_closed():
+    assert valuation.build(0,{},{} )["status"]=="DATA_UNAVAILABLE"
